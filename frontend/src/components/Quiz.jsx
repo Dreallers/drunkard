ca97f541634c61999f3a107d0e98a0f7520fe09c
Karim Rejani <karim.rjn02@gmail.com>
2023-10-26 17:24:30 +0200

    adding the final structure of the quiz section

--- frontend/src/components/Quiz.jsx
+++ frontend/src/components/Quiz.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Score from "./Score";
 
 function Quiz() {
   return (
     <>
       <Navbar />
       <div className="quiz">
-        <p className="title-quiz">Guess who I am</p>
+        <p className="title-quiz">Guess who I am ?</p>
         <ul className="ingredients-quiz">
           <li>Whisky 5cl</li>
           <li>Angostura bitter 4 dashes</li>
           <li>Brown sugar</li>
-          <li>Orange peel 1</li>
+          <li>Orange peel x1</li>
         </ul>
       </div>
+      <Score />
+      <Footer />
     </>
   );
 }
