29533b3addab482b6c434b3a66deaddca845f777
CarolineG85 <caroline.gallix85@gmail.com>
2023-10-27 10:41:02 +0200

    adding a usestate for activated links quizz and mybar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const [isActive, setActive] = useState("");
+  function handleLinkActive(e) {
+    setActive(e.target.value);
+  }
   return (
     <div className="navbar">
-      <NavLink to="/mybar">My Bar</NavLink>
-      <NavLink to="/home">
-        <img src="/cocktail.png" alt="logo-cocktail-bar" />
-      </NavLink>
-      <NavLink to="/quizz">Quizz</NavLink>
+      <button
+        type="button"
+        value="mybar"
+        onClick={(e) => handleLinkActive(e)}
+        className={isActive === "mybar" ? "active" : "notActive"}
+      >
+        <NavLink to="/mybar">My Bar</NavLink>
+      </button>
+      <button type="button">
+        <NavLink to="/home">
+          <img src="/cocktail.png" alt="logo-cocktail-bar" />
+        </NavLink>
+      </button>
+      <button
+        type="button"
+        value="quizz"
+        onClick={(e) => handleLinkActive(e)}
+        className={isActive === "quizz" ? "active" : "notActive"}
+      >
+        <NavLink to="/quizz">Quizz</NavLink>
+      </button>
     </div>
   );
 }
