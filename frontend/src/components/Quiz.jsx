0d8e7e3a530d7b71829cf337a825f5afab7e04f1
Karim Rejani <karim.rjn02@gmail.com>
2023-11-09 10:04:36 +0100

    updating quiz section

--- frontend/src/components/Quiz.jsx
+++ frontend/src/components/Quiz.jsx
@@ -1,19 +1,52 @@
-import React from "react";
-import Score from "./Score";
+import React, { useState, useEffect } from "react";
+import { useRouteLoaderData } from "react-router-dom";
 
 function Quiz() {
+  const cocktails = useRouteLoaderData("App");
+  const [selectedCocktails, setSelectedCocktails] = useState([]);
+
+  const getRandomCocktail = () =>
+    cocktails[Math.floor(Math.random() * cocktails.length)];
+
+  useEffect(() => {
+    if (cocktails && cocktails.length > 0) {
+      const newSelectedCocktails = Array.from({ length: 6 }, getRandomCocktail);
+      setSelectedCocktails(newSelectedCocktails);
+    }
+  }, [cocktails]);
+
+  if (selectedCocktails.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  const [correctCocktail, ...restSelectedCocktails] = selectedCocktails;
+
+  const ingredients = Array.from(
+    { length: 15 },
+    (_, i) => correctCocktail[`drinkIngredient${i + 1}`]
+  ).filter(Boolean);
+
   return (
     <>
       <div className="quiz">
-        <p className="title-quiz">Guess who I am ?</p>
+        <p className="title-quiz">Guess who I am?</p>
         <ul className="ingredients-quiz">
-          <li>Whisky 5cl</li>
-          <li>Angostura bitter 4 dashes</li>
-          <li>Brown sugar</li>
-          <li>Orange peel x1</li>
+          {ingredients.slice(0, 5).map((ingredient) => (
+            <li key={`${correctCocktail.drinkId}-${ingredient}`}>
+              {ingredient}
+            </li>
+          ))}
         </ul>
       </div>
-      <Score />
+      <div className="cards">
+        {restSelectedCocktails.map((cocktail) => (
+          <div className="card" key={cocktail.drinkId}>
+            <div className="cardName">{cocktail.drinkName}</div>
+            <div className="cardIngredients" />
+          </div>
+        ))}
+      </div>
+      <div className="score">Your score:</div>
     </>
   );
 }
